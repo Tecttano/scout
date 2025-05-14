5d3761b642b5a87fb2d0f89e2a7b6d835de46921
Tecttano <chrisferrari0114@gmail.com>
2025-05-14 11:46:14 -0500

    Arguements

--- scout.py
+++ scout.py
@@ -1,9 +1,16 @@
 # 05/14/2025
 import subprocess
+import argparse
 
-domain = input("Enter a domain: ")
-print(f"You entered: {domain}")
+parser = argparse.ArgumentParser(description="Scout - Basic Recon Tool")
 
+# Arguements
+# Domain
+parser.add_argument("-d", "--domain", help="Target domain name")
+# Full Scan
+parser.add_argument("-f", "--full", action="store_true", help="Run a full scan")
+
+# Functions
 # WHOIS
 def run_whois(domain):
         result = subprocess.run(["whois", domain], capture_output=True, text=True)
@@ -17,7 +24,20 @@ def run_ping(domain):
         result = subprocess.run(["ping", "-c", "4", domain], capture_output=True, text=True)
         return result.stdout
 
-print("\n=== INFO ===")
+# Parse
+args = parser.parse_args()
+domain = args.domain
+if not domain:
+        print("You must provide a domain with -d or --domain")
+        exit()
+
+print (f"\nYou entered: {domain}")
+print("\n=== WHOIS INFO ===")
 print(run_whois(domain))
+print("\n=== DIG INFO ===")
 print(run_dig(domain))
-print(run_ping(domain))
+
+if args.full:
+    print("\n=== PING INFO ===")
+    print(run_ping(domain))
+
